ba6cba5eed6a640fb4a2b3f8811eb8565d65138e
Akashbhoi4859 <akashbhoi4859@gmail.com>
2025-05-02 17:46:26 +0530

    Add files via upload

---
+++ COUNTDOWN/timer.js
@@ -0,0 +1,28 @@
+
+let time = 60; 
+const timerElement = document.getElementById('timer');
+
+function updateTimer() {
+  let minutes = Math.floor(time / 60);
+  // console.log(minutes);
+  
+  let seconds = time % 60;
+  // console.log(seconds);
+  
+
+  // Format time
+  minutes = minutes < 10 ? '0' + minutes : minutes;
+  seconds = seconds < 10 ? '0' + seconds : seconds;
+
+  timerElement.innerHTML = `${minutes}:${seconds}`;
+
+  if (time > 0) {
+    time--;
+  } else {
+    clearInterval(countdown);
+    timerElement.innerHTML = "Time's up!";
+  }
+}
+
+updateTimer(); 
+const countdown = setInterval(updateTimer, 1000);
